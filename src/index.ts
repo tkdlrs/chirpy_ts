3b4c47724c1c63ad919b51fe1c29497c5ab283c5
tkdlrs <tkd.lsanchez@gmail.com>
2026-01-18 15:47:09 -0700

    Completes 'Chapter 1: Servers | Lesson 12: Custom Handlers' of the 'Learn HTTP Servers in TypeScript' course from Boot.dev.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
 
+import { Request, Response } from "express";
+
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
 
 
+app.get("/healthz", handlerReadiness);
+
+async function handlerReadiness(req: Request, res: Response): Promise<void> {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send('OK');
+}
