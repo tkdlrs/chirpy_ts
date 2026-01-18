299476501f4af47be67e55348768ada3ce6d0b79
tkdlrs <tkd.lsanchez@gmail.com>
2026-01-18 15:54:37 -0700

    Reviews the Solution Files for 'Chapter 1: Servers | Lesson 12: Custom Handlers'. Made the adjustments that made sense to me. Mainly the setting up a new directory for 'api' and putting the 'readinessHandler' in it's own file in that new directory.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import express from "express";
 
-import { Request, Response } from "express";
+import { handlerReadiness } from "./api/readiness.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use("/app", express.static("./src/app"));
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
-
 
 app.get("/healthz", handlerReadiness);
 
-async function handlerReadiness(req: Request, res: Response): Promise<void> {
-    res.set("Content-Type", "text/plain; charset=utf-8");
-    res.send('OK');
-}
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
