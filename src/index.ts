88ed3c2da7e8b61801013d778eed0a22b36fc410
tkdlrs <tkd.lsanchez@gmail.com>
2026-01-20 18:28:30 -0700

    Completes 'Chapter 3: Architecture | Lesson 1: Monoliths and Decoupling' of the 'Learn HTTP Servers in TypeScript' course.

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,9 @@ const PORT = 8080;
 app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 //
-app.get("/healthz", handlerReadiness);
-app.get("/metrics", handlerMetrics);
-app.get("/reset", handlerReset);
+app.get("/api/healthz", handlerReadiness);
+app.get("/api/metrics", handlerMetrics);
+app.get("/api/reset", handlerReset);
 //
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
