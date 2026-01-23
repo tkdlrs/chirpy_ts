ef2ad4fac54e1665d0c5299ae3239d6afc3bcbd1
tkdlrs <tkd.lsanchez@gmail.com>
2026-01-22 19:34:01 -0700

    Completes 'Chapter 4: JSON | Lesson 2: JSON' of the 'Learn HTTP Servers in TypeScript' course on Boot.dev.

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {
     middlewareLogResponses,
     middlewareMetricsInc
 } from "./api/middleware.js";
+import { handlerValidate } from "./api/validate.js";
 
 const app = express();
 const PORT = 8080;
@@ -15,6 +16,7 @@ app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 //
 app.get("/api/healthz", handlerReadiness);
+app.post("/api/validate_chirp", handlerValidate);
 //
 app.get("/admin/metrics", handlerMetrics);
 app.post("/admin/reset", handlerReset);
