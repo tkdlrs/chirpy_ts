4e25eaf38c5573571bd325a10b9b176610566b17
tkdlrs <tkd.lsanchez@gmail.com>
2026-01-22 19:04:19 -0700

    Completes 'Chapter 4: JSON | Lesson 1: HTTP Clients' of the 'Learn HTTP Servers in TypeScript' course on Boot.dev. Just changed a GET method to be a POST method.

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/api/healthz", handlerReadiness);
 //
 app.get("/admin/metrics", handlerMetrics);
-app.get("/admin/reset", handlerReset);
+app.post("/admin/reset", handlerReset);
 //
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
