e15518fd1325832c1e46fa23cd92194ca1fe29b2
tkdlrs <tkd.lsanchez@gmail.com>
2026-01-23 19:27:22 -0700

    Did 'Chapter 4: JSON | Lesson 3: JSON Middleware' and learned how to use the Express libraries built in middleware for handling JSON request bodies.

--- src/api/chirps.ts
+++ src/api/chirps.ts
@@ -5,30 +5,41 @@ export async function handlerChirpsValidate(req: Request, res: Response) {
     type parameters = {
         body: string;
     };
+    // //
+    // let body = "";
+    // //
+    // req.on("data", (chunk) => {
+    //     body += chunk;
+    // });
     //
-    let body = "";
+    // let params: parameters;
+    // req.on("end", () => {
+    //     try {
+    //         params = JSON.parse(body);
+    //     } catch (e) {
+    //         respondWithError(res, 400, "Invalid JSON");
+    //         return;
+    //     }
+    //     //
+    //     const maxChirpLength = 140;
+    //     if (params.body.length > maxChirpLength) {
+    //         respondWithError(res, 400, "Chirp is too long");
+    //         return;
+    //     }
+    //     //
+    //     respondWithJSON(res, 200, {
+    //         valid: true
+    //     });
+    // });
+    const params: parameters = req.body;
     //
-    req.on("data", (chunk) => {
-        body += chunk;
-    });
-    //
-    let params: parameters;
-    req.on("end", () => {
-        try {
-            params = JSON.parse(body);
-        } catch (e) {
-            respondWithError(res, 400, "Invalid JSON");
-            return;
-        }
-        //
-        const maxChirpLength = 140;
-        if (params.body.length > maxChirpLength) {
-            respondWithError(res, 400, "Chirp is too long");
-            return;
-        }
-        //
-        respondWithJSON(res, 200, {
-            valid: true
-        });
+    const maxChirpLength = 140;
+    if (params.body.length > maxChirpLength) {
+        respondWithError(res, 400, "Chirp is too long");
+        return;
+    }
+    // 
+    respondWithJSON(res, 200, {
+        valid: true
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,8 @@ const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
+app.use(express.json());
+//
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 //
 app.get("/api/healthz", handlerReadiness);
