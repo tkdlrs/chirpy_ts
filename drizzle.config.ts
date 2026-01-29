0d2e120d6aad626f49d4e0c901108da48da10f1e
tkdlrs <tkd.lsanchez@gmail.com>
2026-01-28 17:50:11 -0700

    Completes 'Chapter 6: Storage | Lesson 3: Drizzle Queries' of the 'Learn HTTP Servers in TypeScript' course on Boot.dev.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "drizzle-kit";
 
+import { config } from "./src/config"
+
 export default defineConfig({
     schema: "src/db/schema.ts",
     out: "src/db/migrations",
     dialect: "postgresql",
     dbCredentials: {
-        url: ""
-    }
-})
+        url: config.dbURL
+    },
+});
+//
--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,21 @@
-
+//
 type APIConfig = {
     fileServerHits: number;
+    dbURL: string;
 }
-
+//
+process.loadEnvFile();
+//
+function envOrThrow(key: string) {
+    const value = process.env[key];
+    if (!value) {
+        throw new Error(`Environmental variable ${key} is not set`);
+    }
+    return value;
+}
+//
 export const config: APIConfig = {
-    fileServerHits: 0
+    fileServerHits: 0,
+    dbURL: envOrThrow("DB_URL")
 };
+//
